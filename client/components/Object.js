a2f65de7fb92609e2cac999c31d9e25b3149676e
Cassie Rosner <cerosner@gmail.com>
2019-03-18 13:01:43 -0400

    removes comment

--- client/components/Object.js
+++ client/components/Object.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 
-// object coords in store so Game component can use
 class Object extends Component {
   constructor(props) {
     super(props)
